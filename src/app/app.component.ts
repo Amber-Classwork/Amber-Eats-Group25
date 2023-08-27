93c55872607df407b3bb700938a7e35ddc6008fa
Aldaine-Legion <vainedev@gmail.com>
2023-08-27 03:26:13 -0500

    added ghpages cli and minified details component css as well as changed title of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { CartService } from './Services/cart.service';
 import { LoadingService } from './Services/loading.service';
 @Component({
   selector: 'app-root',
@@ -8,7 +6,7 @@ import { LoadingService } from './Services/loading.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-http-crud1';
+  title = 'Amber Eats';
   transparent = true;
   constructor(private loadingService: LoadingService) {}
 
